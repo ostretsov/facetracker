be67638d0a2a7a5ef3bfafa139637fa8e3d0878b
alexis031182 <alexis031182@gmail.com>
2015-05-24 10:55:11 +0300

    Add face detection code.

--- acapturethread.cpp
+++ acapturethread.cpp
@@ -15,7 +15,24 @@
 // Constructor.
 // ========================================================================== //
 ACaptureThread::ACaptureThread(QObject *parent)
-    : QThread(parent), _dev_idx(0), _hidden_capture(true) {}
+    : QThread(parent), _dev_idx(0), _det_min(20), _det_max(50)
+    , _hidden_capture(true), _hidden_detect(true) {}
+
+
+// ========================================================================== //
+// Get detector minimum size.
+// ========================================================================== //
+int ACaptureThread::detectorMinSize() {
+    QMutexLocker locker(&_mutex); return _det_min;
+}
+
+
+// ========================================================================== //
+// Get detector maximum size.
+// ========================================================================== //
+int ACaptureThread::detectorMaxSize() {
+    QMutexLocker locker(&_mutex); return _det_max;
+}
 
 
 // ========================================================================== //
@@ -27,10 +44,42 @@ bool ACaptureThread::isHiddenCapture() {
 
 
 // ========================================================================== //
+// Get detection is hidden.
+// ========================================================================== //
+bool ACaptureThread::isHiddenDetect() {
+    QMutexLocker locker(&_mutex); return _hidden_detect;
+}
+
+
+// ========================================================================== //
+// Set detector minimum size.
+// ========================================================================== //
+void ACaptureThread::setDetectorMinSize(int min) {
+    QMutexLocker locker(&_mutex); _det_min = min;
+}
+
+
+// ========================================================================== //
+// Set detector maximum size.
+// ========================================================================== //
+void ACaptureThread::setDetectorMaxSize(int max) {
+    QMutexLocker locker(&_mutex); _det_max = max;
+}
+
+
+// ========================================================================== //
 // Set capture hidden.
 // ========================================================================== //
 void ACaptureThread::setCaptureHidden(bool hidden) {
     QMutexLocker locker(&_mutex); _hidden_capture = hidden;
+}
+
+
+// ========================================================================== //
+// Set detection hidden.
+// ========================================================================== //
+void ACaptureThread::setDetectHidden(bool hidden) {
+    QMutexLocker locker(&_mutex); _hidden_detect = hidden;
 }
 
 
@@ -73,16 +122,58 @@ void ACaptureThread::run() {
 
     delete dst_file;
 
+    cv::Mat acc_mat;
+
     while(!isInterruptionRequested()) {
         cv::Mat src_mat;
         capture >> src_mat;
 
-        if(!isHiddenCapture()) {
+        cv::Mat gry_mat;
+        cv::cvtColor(src_mat, gry_mat, cv::COLOR_BGR2GRAY);
+
+        if(acc_mat.rows != gry_mat.rows || acc_mat.cols != gry_mat.cols)
+            acc_mat = cv::Mat(gry_mat.size(), CV_64F);
+
+        cv::accumulateWeighted(gry_mat, acc_mat, 0.75);
+        cv::convertScaleAbs(acc_mat, gry_mat);
+
+        _mutex.lock();
+        const int det_min = _det_min;
+        const int det_max = _det_max;
+        _mutex.unlock();
+
+        const int hrz_min = gry_mat.cols * det_min / 100;
+        const int vrt_min = gry_mat.rows * det_min / 100;
+        const int hrz_max = gry_mat.cols * det_max / 100;
+        const int vrt_max = gry_mat.rows * det_max / 100;
+
+        std::vector<cv::Rect> rois;
+        classifier.detectMultiScale(gry_mat, rois, 1.1, 3, 0
+            , cv::Size(hrz_min,vrt_min), cv::Size(hrz_max,vrt_max));
+
+        if(isHiddenCapture()) emit captured();
+        else {
+            if(rois.size() > 0)
+                cv::rectangle(src_mat, rois.at(0), cv::Scalar(0,255,0), 1);
+
             QImage img(src_mat.data, src_mat.cols, src_mat.rows
                 , src_mat.step, QImage::Format_RGB888);
 
             emit captured(img.convertToFormat(
                 QImage::Format_ARGB32_Premultiplied));
+        }
+
+        if(rois.size() > 0) {
+            if(isHiddenDetect()) emit detected();
+            else {
+                cv::Mat roi_mat = src_mat(rois.at(0));
+
+                QImage img(roi_mat.data, roi_mat.cols, roi_mat.rows
+                    , roi_mat.step, QImage::Format_RGB888);
+
+                emit detected(img.convertToFormat(
+                    QImage::Format_ARGB32_Premultiplied));
+            }
         }
     }
 }
--- acapturethread.h
+++ acapturethread.h
@@ -8,7 +8,11 @@ class ACaptureThread : public QThread {
     Q_OBJECT
 
     signals:
+        void captured();
         void captured(const QImage &img);
+
+        void detected();
+        void detected(const QImage &img);
 
     public:
         //! Constructor.
@@ -17,12 +21,30 @@ class ACaptureThread : public QThread {
         //! Destructor.
         virtual ~ACaptureThread() {}
 
+        //! Get detector minimum size.
+        int detectorMinSize();
+
+        //! Get detector maximum size.
+        int detectorMaxSize();
+
         //! Get capture is hidden.
         bool isHiddenCapture();
 
+        //! Get detection is hidden.
+        bool isHiddenDetect();
+
     public slots:
+        //! Set detector minimum size.
+        void setDetectorMinSize(int min);
+
+        //! Set detector maximum size.
+        void setDetectorMaxSize(int max);
+
         //! Set capture hidden.
         void setCaptureHidden(bool hidden);
+
+        //! Set detection hidden.
+        void setDetectHidden(bool hidden);
 
     protected:
         //! Run.
@@ -31,7 +53,9 @@ class ACaptureThread : public QThread {
     private:
         int _dev_idx;
 
-        bool _hidden_capture;
+        int _det_min, _det_max;
+
+        bool _hidden_capture, _hidden_detect;
 
         QMutex _mutex;
 
