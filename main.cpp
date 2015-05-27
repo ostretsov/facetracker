c2010ea2fdda1816aea7ed6027cdf575f5d1e75e
alexis031182 <alexis031182@gmail.com>
2015-05-27 16:02:30 +0300

    Remove check version code from main module.

--- main.cpp
+++ main.cpp
@@ -3,34 +3,7 @@
 
 #include <QtWidgets/QApplication>
 
-#include "helpers/asettingshelper.h"
-
 #include "aservicecontroller.h"
-
-// ========================================================================== //
-// Check application version.
-// ========================================================================== //
-void checkApplicationVersion(const QCoreApplication &app) {
-    QStringList versions;
-    versions << QStringLiteral("1.0");
-
-    const QString version
-        = ASettingsHelper::value(QStringLiteral("version")).toString();
-
-    if(versions.contains(version)) {
-        if(app.applicationVersion() != version) {
-            ASettingsHelper::setValue(QStringLiteral("version")
-                , app.applicationVersion());
-        }
-
-        return;
-    }
-
-    ASettingsHelper::clear();
-    ASettingsHelper::setValue(QStringLiteral("version")
-        , app.applicationVersion());
-}
-
 
 // ========================================================================== //
 //
@@ -44,14 +17,13 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     app.setQuitOnLastWindowClosed(false);
     app.setProperty("conf"
-        , app.applicationDirPath() + QLatin1String("/facetracker.ini"));
+        , app.applicationDirPath() + QLatin1Char('/')
+            + app.applicationName() + QLatin1String(".ini"));
 
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
 
     QLoggingCategory::setFilterRules(
         QStringLiteral("qt.network.ssl.warning=false"));
-
-    checkApplicationVersion(app);
 
     AServiceController::instance();
 
